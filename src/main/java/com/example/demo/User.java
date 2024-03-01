31fcd7291888a51882245193c79fb03722eb5388
anadokaza <anadokaza@Anas-Laptop.local>
2024-03-01 13:15:53 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//increments
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+}
---
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "User" (
+                      id SERIAL PRIMARY KEY,
+                      username VARCHAR(50) NOT NULL,
+                      email VARCHAR(100) NOT NULL,
+                      password VARCHAR(100) NOT NULL
+);
+
+
+
