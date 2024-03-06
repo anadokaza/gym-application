c5ea9d60a6ea787f8f2b0e486138875a830b6c40
gorazdbiskoski <gorazdbiskoskii@gmail.com>
2024-03-06 14:00:15 +0100

    Update User.java

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -12,27 +12,11 @@ import lombok.NoArgsConstructor;
 @Builder
 @Entity
 @Table(name = "users")
-public class User {
+public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
     private String email;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
