c00f15d3824bfc652bd85589f529c1ba46a84382
gorazdbiskoski <126030318+gorazdbiskoski@users.noreply.github.com>
2024-03-08 13:26:29 +0100

    Update User.java

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @Entity
 @Table(name = "users")
-public class User{
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
