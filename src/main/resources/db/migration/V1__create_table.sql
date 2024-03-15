41c4654fd25a5734eff3cae47a1c61690adb8d65
Aleksandar Nikov <aleksandar.nikov@netcetera.com>
2024-03-15 13:02:11 +0100

    flyway plugin version user table renamed to users

--- src/main/resources/db/migration/V1__create_table.sql
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "User" (
+CREATE TABLE "users" (
                       id SERIAL PRIMARY KEY,
                       username VARCHAR(50) NOT NULL,
                       email VARCHAR(100) NOT NULL,
--- src/main/resources/db/migration/V2__insert_table.sql
+++ src/main/resources/db/migration/V2__insert_table.sql
@@ -1,2 +1,4 @@
-INSERT INTO "User"( USERNAME, EMAIL, PASSWORD)
-values ('user1', 'ana@gmail.com','pass1');
+INSERT INTO "users"( USERNAME, EMAIL, PASSWORD)
+  values ('user1', 'ana@gmail.com','pass1');
+INSERT INTO "users"( USERNAME, EMAIL, PASSWORD)
+values ('user2', 'gorazd@gmail.com','pass2');
